272d0e305f49796368d6cf881753a7fe9042122d
Blaise Taylor <btaylor@bPolicySolutions.com>
2026-06-19 06:33:11 -0400

    Add project files.

---
+++ LogicBuilder.App.Web.Utils/HttpClientHelper.cs
@@ -0,0 +1,26 @@
+﻿using LogicBuilder.App.Web.Utils.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilder.App.Web.Utils
+{
+    public class HttpClientHelper : IHttpClientHelper
+    {
+        public Task<TResult> GetAsync<TResult>(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResult> PostAsync<TResult>(string url, string jsonObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResult> PutAsync<TResult>(string url, string jsonObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LogicBuilder.App.Web.Utils/Interfaces/IHttpClientHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace LogicBuilder.App.Web.Utils.Interfaces
+{
+    public interface IHttpClientHelper
+    {
+        Task<TResult> GetAsync<TResult>(string url);
+        Task<TResult> PostAsync<TResult>(string url, string jsonObject);
+        Task<TResult> PutAsync<TResult>(string url, string jsonObject);
+    }
+}
